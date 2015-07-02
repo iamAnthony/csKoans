0a31a199be2bec56026a5a8b7f0257c934371e7f
Anthony Aubrée <anthony.aubree@etu.univ-lyon1.fr>
2015-07-02 09:54:28 +0200

    Le test sur la classe réalisant une somme passe.

---
+++ FinalTest/Somme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FinalTest
+{
+    public class Somme
+    {
+        public bool PeutCalculer(string p0)
+        {
+            return p0.Contains("+") && !p0.Contains("/") && !p0.Contains("-") && !p0.Contains("*");
+        }
+
+        public int Calculer(string s)
+        {
+            if (PeutCalculer(s))
+            {
+                var a = int.Parse(s.Split('+').GetValue(0).ToString());
+                var b = int.Parse(s.Split('+').GetValue(1).ToString());
+                return a + b;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
