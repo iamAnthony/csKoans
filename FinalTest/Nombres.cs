2b61a760841a42ffa49a1c81eed3c0376d5c6671
Anthony Aubrée <anthony.aubree@etu.univ-lyon1.fr>
2015-07-02 10:42:53 +0200

    Le test pour récuperer les nombres pairs fonctionnent (LinqTests)

---
+++ FinalTest/Nombres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FinalTest
+{
+    public class Nombres
+    {
+        private readonly IEnumerable<KeyValuePair<string, int>> _keyValuePairs;
+
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            _keyValuePairs = keyValuePairs;
+        }
+
+        public IEnumerable<int> NombresPairs
+        {
+            get { return _keyValuePairs.Where(valeur => (valeur.Value%2 == 0)).Select(valeur=>valeur.Value); }
+        }
+    }
+}
