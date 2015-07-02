7efaa9998a19d3730d845d5464edfc27b34b3c83
Anthony Aubrée <anthony.aubree@etu.univ-lyon1.fr>
2015-07-02 09:26:17 +0200

    Le test sur la définition d'un type référence passe.

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,30 @@
+﻿namespace FinalTest
+{
+    public class TypeReference
+    {
+        protected bool Equals(TypeReference other)
+        {
+            return _valeur == other._valeur;
+        }
+
+        public override int GetHashCode()
+        {
+            return _valeur;
+        }
+
+        private int _valeur;
+
+        public TypeReference(int i)
+        {
+            this._valeur = i;
+        }
+
+       public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TypeReference) obj);
+        }
+    }
+}
