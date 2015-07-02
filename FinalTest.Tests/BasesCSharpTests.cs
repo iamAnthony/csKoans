309d55c598d3ce9aba70822064e935b3cf888298
Anthony Aubrée <anthony.aubree@etu.univ-lyon1.fr>
2015-07-02 09:20:15 +0200

    Le test DefinirUnTypeValeur passe.

--- FinalTest.Tests/BasesCSharpTests.cs
+++ FinalTest.Tests/BasesCSharpTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NFluent;
+﻿using NFluent;
 using NUnit.Framework;
 
 namespace FinalTest.Tests
@@ -8,30 +7,30 @@ namespace FinalTest.Tests
     public class BasesCSharpTests
     {
         [Test]
-        public void CeTestDoitPasserSiEnvironnementOK()
+        public void CeTestDoitPasserSiEnvironnementOk()
         {
             Check.That(true).IsTrue();
         }
 
-        //[Test]
-        //public void DéfinirUnTypeValeur()
-        //{
-        //    var valeur1 = new TypeValeur(12);
-        //    var valeur2 = new TypeValeur(12);
+        [Test]
+        public void DéfinirUnTypeValeur()
+        {
+            var valeur1 = new TypeValeur(12);
+            var valeur2 = new TypeValeur(12);
 
-        //    Check.That(valeur1).IsEqualTo(valeur2);
-        //    Check.That(typeof(TypeValeur).IsValueType).IsTrue();
-        //}
+            Check.That(valeur1).IsEqualTo(valeur2);
+            Check.That(typeof(TypeValeur).IsValueType).IsTrue();
+        }
 
-        //[Test]
-        //public void DéfinirUnTypeRéférenceAvecEgalitéDeuxInstancesAyantLesMêmesPropriétés()
-        //{
-        //    var valeur1 = new TypeReference(12);
-        //    var valeur2 = new TypeReference(12);
+        [Test]
+        public void DéfinirUnTypeRéférenceAvecEgalitéDeuxInstancesAyantLesMêmesPropriétés()
+        {
+            var valeur1 = new TypeReference(12);
+            var valeur2 = new TypeReference(12);
 
-        //    Check.That(valeur1).IsEqualTo(valeur2);
-        //    Check.That(typeof(TypeReference).IsValueType).IsFalse();
-        //}
+            Check.That(valeur1).IsEqualTo(valeur2);
+            Check.That(typeof(TypeReference).IsValueType).IsFalse();
+        }
 
         //[Test]
         //public void DéfinirUneClasseRealisantUneMultiplication()
---
+++ FinalTest/TypeValeur.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FinalTest
+{
+    public struct TypeValeur
+    {
+        private int _valeur;
+
+        public TypeValeur(int i)
+        {
+            this._valeur = i;
+        }
+    }
+}
