a4c7fe4f895b4e1c8deb11e61e494f5a7e0ed08f
Anthony Aubrée <anthony.aubree@etu.univ-lyon1.fr>
2015-07-02 10:57:45 +0200

    Les tests pour recuperer une chaine avec les nombres impairs passent.

--- FinalTest/Nombres.cs
+++ FinalTest/Nombres.cs
@@ -17,5 +17,17 @@ namespace FinalTest
         {
             get { return _keyValuePairs.Where(valeur => (valeur.Value%2 == 0)).Select(valeur=>valeur.Value); }
         }
+
+        public String TexteNombresImpairs
+        {
+            get
+            {
+                return
+                    _keyValuePairs.Where(valeur => (valeur.Value%2 != 0))
+                        .OrderBy(valeur => valeur.Value)
+                        .Select(valeur => valeur.Key)
+                        .Aggregate((s, s1) => s + ", " + s1);
+            }
+        }
     }
 }
